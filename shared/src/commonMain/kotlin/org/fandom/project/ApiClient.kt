b04cd48d6ebd8401b7042407fcd93085af295cd1
gunesezgikus <gunesezgi99@gmail.com>
2026-06-26 14:19:13 +0200

    feat: add ApiClient to fecth articles from API

---
+++ shared/src/commonMain/kotlin/org/fandom/project/ApiClient.kt
@@ -0,0 +1,33 @@
+package org.fandom.project
+
+import io.ktor.client.*
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+
+
+class ApiClient {
+    private val client = HttpClient{
+        install(ContentNegotiation){
+            json(Json{
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+    suspend fun fetchArticles():List<Article>{
+        return try {
+            val response = client.get("https://services.fandom.com/mobile-sidekick/trending/articles")
+            response.body()
+        } catch (e: Exception){
+            println("API error: ${e.message}")
+            emptyList()
+        }
+    }
+
+
+}
